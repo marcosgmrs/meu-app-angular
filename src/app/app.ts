f6179ac6aa40d0c15db53e04bffb40df7783f9ca
Marcos <marcosgmrs@outlook.com>
2026-03-27 19:37:49 -0300

    primeiro app Angular rodando

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('meu-app-angular');
+  protected readonly title = signal('Marcos');
 }
