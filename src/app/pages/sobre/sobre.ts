9205d4eb5dc1464c05743fb427d5c20e1a2d6d1a
Marcos <marcosgmrs@outlook.com>
2026-03-31 00:28:08 -0300

    estiliza paginas home, tarefas, sobre e login

--- src/app/pages/sobre/sobre.ts
+++ src/app/pages/sobre/sobre.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sobre.html',
   styleUrl: './sobre.css',
 })
